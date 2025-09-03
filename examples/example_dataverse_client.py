a4a647a0544e4a8fbcf9395569c8292fbc85dd33
Patrick Latimer <patrick.latimer@alleninstitute.org>
2025-09-02 17:28:26 -0700

    Clean up rest client code

---
+++ examples/example_dataverse_client.py
@@ -0,0 +1,30 @@
+"""Examples for using the Dataverse client"""
+
+from random import randint
+
+from dataverse_client import DataverseConfig, DataverseRestClient
+
+
+def example_using_dataverse_client():
+    """Example usage of the DataverseRestClient with mice table"""
+    config = DataverseConfig()
+    client = DataverseRestClient(config)
+
+    mouse_table = "crb81_dim_mice_bases"
+    mouse_id = "614174"
+    mouse_guid = "fe057d74-8683-f011-b4cb-6045bd03524b"
+    mouse = client.get_entry(mouse_table, {"crb81_mouse_id": mouse_id})
+    print(mouse)
+    mouse = client.get_entry(mouse_table, mouse_guid)
+    print(mouse)
+
+    updated_mouse = client.update_entry(
+        mouse_table,
+        {"crb81_mouse_id": mouse_id},
+        {"crb81_full_genotype": "UpdatedGenotype_" + str(randint(1, 100))},
+    )
+    print(updated_mouse)
+
+
+if __name__ == "__main__":
+    example_using_dataverse_client()
--- src/dataverse_client/__init__.py
+++ src/dataverse_client/__init__.py
@@ -1,2 +1,8 @@
 """Init package"""
+
 __version__ = "0.0.0"
+
+from .rest_client import DataverseConfig, DataverseRestClient
+
+
+__all__ = ["DataverseConfig", "DataverseRestClient"]
